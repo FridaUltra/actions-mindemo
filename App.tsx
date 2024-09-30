7ad6b0c27d5c197e3cfcc6a61be43fd8a02d679e
FridaUltra <5carlsonfrida@gmail.com>
2024-09-30 13:00:58 +0200

    refactor: created a rootstacknavigation component

--- App.tsx
+++ App.tsx
@@ -1,52 +1,10 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Button, StyleSheet } from 'react-native';
-import DetailsScreen from './screens/DetailsScreen';
-import HomeScreen from './screens/HomeScreens';
-import SettingsScreen from './screens/SettingsScreen';
-
-export type RootStackParamList = {
-	Home: undefined;
-	Details: undefined;
-	Settings: undefined;
-};
-
-const RootStack = createNativeStackNavigator<RootStackParamList>();
+import RootStackNavigator from './navigators/RootStacknavigator';
 
 export default function App() {
 	return (
 		<NavigationContainer>
-			<RootStack.Navigator
-				screenOptions={({ navigation }) => ({
-					headerRight: () => (
-						<Button
-							title="Settings"
-							onPress={() => navigation.navigate('Settings')}
-						/>
-					),
-				})}>
-				<RootStack.Screen
-					name="Home"
-					component={HomeScreen}
-				/>
-				<RootStack.Screen
-					name="Details"
-					component={DetailsScreen}
-				/>
-				<RootStack.Screen
-					name="Settings"
-					component={SettingsScreen}
-				/>
-			</RootStack.Navigator>
+			<RootStackNavigator />
 		</NavigationContainer>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
