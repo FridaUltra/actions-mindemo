c699a336048959ecac672cf2e81587152faa6cb6
FridaUltra <5carlsonfrida@gmail.com>
2024-09-30 13:30:49 +0200

    prettier --write

--- App.tsx
+++ App.tsx
@@ -6,47 +6,39 @@ import HomeScreen from './screens/HomeScreens';
 import SettingsScreen from './screens/SettingsScreen';
 
 export type RootStackParamList = {
-	Home: undefined;
-	Details: undefined;
-	Settings: undefined;
+  Home: undefined;
+  Details: undefined;
+  Settings: undefined;
 };
 
 const RootStack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
-	return (
-		<NavigationContainer>
-			<RootStack.Navigator
-				screenOptions={({ navigation }) => ({
-					headerRight: () => (
-						<Button
-							title="Settings"
-							onPress={() => navigation.navigate('Settings')}
-						/>
-					),
-				})}>
-				<RootStack.Screen
-					name="Home"
-					component={HomeScreen}
-				/>
-				<RootStack.Screen
-					name="Details"
-					component={DetailsScreen}
-				/>
-				<RootStack.Screen
-					name="Settings"
-					component={SettingsScreen}
-				/>
-			</RootStack.Navigator>
-		</NavigationContainer>
-	);
+  return (
+    <NavigationContainer>
+      <RootStack.Navigator
+        screenOptions={({ navigation }) => ({
+          headerRight: () => (
+            <Button
+              title="Settings"
+              onPress={() => navigation.navigate('Settings')}
+            />
+          ),
+        })}
+      >
+        <RootStack.Screen name="Home" component={HomeScreen} />
+        <RootStack.Screen name="Details" component={DetailsScreen} />
+        <RootStack.Screen name="Settings" component={SettingsScreen} />
+      </RootStack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
