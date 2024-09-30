cdff3bea8a7ae87b6108c7b62e42db290b5e4f1d
FridaUltra <5carlsonfrida@gmail.com>
2024-09-30 15:49:20 +0200

    refactor iconbutton

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -1,7 +1,11 @@
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { Pressable } from 'react-native';
+import { RootStackParamList } from '../navigators/RootStacknavigator';
 
-export default function IconButton({ navigation }) {
+type Props = NativeStackScreenProps<RootStackParamList>;
+
+export default function IconButton({ navigation }: Props) {
   return (
     <Pressable onPress={() => navigation.navigate('Settings')}>
       <MaterialIcons name="settings" size={24} color="black" />
--- navigators/RootStacknavigator.tsx
+++ navigators/RootStacknavigator.tsx
@@ -1,40 +1,27 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Button } from 'react-native';
+import IconButton from '../components/IconButton';
 import DetailsScreen from '../screens/DetailsScreen';
 import HomeScreen from '../screens/HomeScreens';
 import SettingsScreen from '../screens/SettingsScreen';
 
 export type RootStackParamList = {
-	Home: undefined;
-	Details: undefined;
-	Settings: undefined;
+  Home: undefined;
+  Details: undefined;
+  Settings: undefined;
 };
 
 const RootStack = createNativeStackNavigator<RootStackParamList>();
 
 export default function RootStackNavigator() {
-	return (
-		<RootStack.Navigator
-			screenOptions={({ navigation }) => ({
-				headerRight: () => (
-					<Button
-						title="Settings"
-						onPress={() => navigation.navigate('Settings')}
-					/>
-				),
-			})}>
-			<RootStack.Screen
-				name="Home"
-				component={HomeScreen}
-			/>
-			<RootStack.Screen
-				name="Details"
-				component={DetailsScreen}
-			/>
-			<RootStack.Screen
-				name="Settings"
-				component={SettingsScreen}
-			/>
-		</RootStack.Navigator>
-	);
+  return (
+    <RootStack.Navigator
+      screenOptions={(props) => ({
+        headerRight: () => <IconButton {...props} />,
+      })}
+    >
+      <RootStack.Screen name="Home" component={HomeScreen} />
+      <RootStack.Screen name="Details" component={DetailsScreen} />
+      <RootStack.Screen name="Settings" component={SettingsScreen} />
+    </RootStack.Navigator>
+  );
 }
